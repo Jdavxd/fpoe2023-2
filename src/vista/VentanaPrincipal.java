b668e08d9775595735b82d5c492b8e7507fe5e06
David <julian.david.rengifo@correounivalle.edu.co>
2023-10-08 02:12:46 -0500

    ajustes

--- src/vista/VentanaPrincipal.java
+++ src/vista/VentanaPrincipal.java
@@ -5,14 +5,11 @@
 package vista;
 
 
-import modelo.Jugador;
 
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Image;
 import java.awt.Toolkit;
-//import java.awt.event.ActionEvent;
-//import java.awt.event.ActionListener;
 //import java.awt.event.KeyEvent;
 //import java.awt.event.KeyListener;
 import javax.swing.JButton;
@@ -109,6 +106,10 @@ public final class VentanaPrincipal extends JFrame {
 	setIconImage(miIcono);
                
     }
+        
+        private void boton(java.awt.event.ActionEvent evt){
+            
+        }
     
     
 }
